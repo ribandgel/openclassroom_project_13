6ce339f6393cde5e732e3d9148621a38894c5358
Andgel <a.ribeiro@pictia.io>
2022-06-09 19:31:23 +0200

    refactoring 2.0

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -1,13 +1,6 @@
+from django.conf.urls import include, url
 from django.contrib import admin
 from django.urls import path
-
 from . import views
 
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('lettings/', views.lettings_index, name='lettings_index'),
-    path('lettings/<int:letting_id>/', views.letting, name='letting'),
-    path('profiles/', views.profiles_index, name='profiles_index'),
-    path('profiles/<str:username>/', views.profile, name='profile'),
-    path('admin/', admin.site.urls),
-]
+urlpatterns = [path('', views.index, name='index'), path("admin/", admin.site.urls), url("lettings/", include("oc_lettings_site.lettings.urls")), url("profiles/", include("oc_lettings_site.profiles.urls"))]
