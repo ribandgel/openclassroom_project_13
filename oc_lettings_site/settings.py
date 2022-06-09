2bbc28a9e7af75e2530fbd4cab0fef54a4bbb15c
ribandgel <ribeiroandgel@gmx.fr>
2022-06-09 19:31:23 +0200

    refactoring 2.0

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -26,6 +26,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'oc_lettings_site.lettings',
+    'oc_lettings_site.profiles',
 ]
 
 MIDDLEWARE = [
