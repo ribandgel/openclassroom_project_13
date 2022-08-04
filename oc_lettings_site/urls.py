77878a879d3dc55c616013220fb56e1e208e3571
Andgel Ribeiro <andgelribeiro@gmail.com>
2022-08-04 17:56:39 +0200

    Sentry config

--- oc_lettings_site/urls.py
+++ oc_lettings_site/urls.py
@@ -3,8 +3,12 @@ from django.contrib import admin
 from django.urls import path
 from . import views
 
+def trigger_error(request):
+    division_by_zero = 1 / 0
+
 urlpatterns = [
     path("", views.index, name="index"),
+    path('sentry-debug/', trigger_error),
     path("admin/", admin.site.urls),
     path("lettings/", include("oc_lettings_site.lettings.urls")),
     path("profiles/", include("oc_lettings_site.profiles.urls")),
