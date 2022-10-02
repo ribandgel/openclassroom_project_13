4390e942356361789a651a698067356ecb00479a
Andgel Ribeiro <andgelribeiro@gmail.com>
2022-10-02 17:27:31 +0200

    Lint

--- oc_lettings_site/admin.py
+++ oc_lettings_site/admin.py
@@ -1,8 +1,7 @@
 from django.contrib import admin
 
-from oc_lettings_site.lettings.models import Letting, Address
+from oc_lettings_site.lettings.models import Address, Letting
 from oc_lettings_site.profiles.models import Profile
-
 
 admin.site.register(Letting)
 admin.site.register(Address)
